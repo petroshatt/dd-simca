8a6979707257c511396daabd5c715118d9f34a5c
Petros Chatzitoulousis <petroshatt@gmail.com>
2023-06-02 21:05:01 +0300

    First Commit

--- ddsimca_test.py
+++ ddsimca_test.py
@@ -5,9 +5,6 @@ import pandas as pd
 from ddsimca import DDSimca
 
 if __name__ == '__main__':
-    # X = [[2, 0, 2],
-    #      [0, 1, 0],
-    #      [0, 0, 0]]
 
     X = pd.read_csv("../matlab_dataset.csv")
 
